b37cdd90e1216eae73c86c8d66dbe57ca158f7b1
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-12-27 13:31:13 -0500

    first test

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -8,11 +8,18 @@ from .. import layer
 
 class TestConvolutionInitialization(TestCase):
     def test_works(self):
-        conv = layer.Convolution(
-            np.zeros((1, 3, 3)),
-            np.zeros((3,)),
-            3,
-            1,
-            1,
-            mock.Mock()
-        )
+        args = {
+            'filters': np.zeros((1, 3, 3)),
+            'biases': np.zeros((3,)),
+            'size': 3,
+            'stride': 1,
+            'padding': 1,
+            'activation': mock.Mock(),
+        }
+        conv = layer.Convolution(**args)
+
+        for k, v in args.items():
+            if k == 'filters' or k == 'biases':
+                self.assertTrue(np.array_equal(v, getattr(conv, k)))
+            else:
+                self.assertEqual(v, getattr(conv, k))
